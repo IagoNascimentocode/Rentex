af1dbaca551ca3ec29d1b5883ec515f3abebc00d
IagoNascimentocode <iago_ns@hotmail.com>
2021-04-21 21:22:02 -0300

    Provider para data

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,11 +1,13 @@
 import dayjs from "dayjs"
 
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory"
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/DayjsDateProvider"
 import { AppError } from "@shared/errors/AppError"
 import { CreateRentalUseCase } from "./CreateRentalUseCase"
 
 let createRentalUseCase: CreateRentalUseCase
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory
+let dayjsProvider: DayjsDateProvider;
 
 describe("Create Rental", () => {
 
@@ -13,7 +15,8 @@ describe("Create Rental", () => {
 
  beforeEach(() => {
   rentalsRepositoryInMemory = new RentalsRepositoryInMemory()
-  createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  dayjsProvider = new DayjsDateProvider()
+  createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayjsProvider);
  })
 
  it("Should be able to create a new rental", async () => {
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,11 +1,9 @@
-import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc";
-
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
-dayjs.extend(utc);
+
 
 interface IRequest {
  user_id: string;
@@ -16,33 +14,32 @@ interface IRequest {
 class CreateRentalUseCase {
 
  constructor(
-  private rentalsRepository: IRentalsRepository
-
+  private rentalsRepository: IRentalsRepository,
+  private dateProvider: IDateProvider
  ) { }
 
  async execute({ user_id, car_id, expected_return_date }: IRequest): Promise<Rental> {
 
   const minimumHour = 24;
 
-  //Nao deve ser possivel cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
   const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(car_id);
 
   if (carUnavailable) {
    throw new AppError("Car is unavailable")
   }
 
-  //Nao deve ser possivel cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
-
   const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(user_id);
 
   if (rentalOpenToUser) {
    throw new AppError("There's a rental in progress for user!")
   }
-  //O aluguel deve ter duração minima de 24 horas.
 
-  const expectedReturnDateFormat = dayjs(expected_return_date).utc().local().format();
-  const dateNow = dayjs().utc().local().format();
-  const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours")
+  const dateNow = this.dateProvider.dateNow();
+
+  const compare = this.dateProvider.compareInHours(
+   dateNow,
+   expected_return_date,
+  )
 
   if (compare < minimumHour) {
    throw new AppError("Invalid return time.")

43b673385ad649db6e34ff966f43b4af64dda285
IagoNascimentocode <iago_ns@hotmail.com>
2021-06-11 17:19:16 -0300

    Upload utilizando S3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,4 +1,5 @@
 import { container } from "tsyringe";
+import "dotenv/config";
 
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
@@ -8,6 +9,7 @@ import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMai
 
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider"
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 
 container.registerSingleton<IDateProvider>(
  "DayjsDateProvider",
@@ -17,8 +19,14 @@ container.registerInstance<IMailProvider>(
  "EtherealMailProvider",
  new EtherealMailProvider()
 )
+
+const diskStorage = {
+ local: LocalStorageProvider,
+ s3: S3StorageProvider
+}
+
 container.registerSingleton<IStorageProvider>(
  "StorageProvider",
- LocalStorageProvider
+ diskStorage[process.env.disk]
 
 )

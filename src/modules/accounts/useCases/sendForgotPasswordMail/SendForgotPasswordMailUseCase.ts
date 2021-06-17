224efaf3548cd7e277c64ae607ee899612399e41
IagoNascimentocode <iago_ns@hotmail.com>
2021-06-16 22:01:26 -0300

    Provider SESMail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -19,7 +19,7 @@ class SendForgotPasswordMailUseCase {
   private usersTokensRepository: IUsersTokensRepository,
   @inject("DayjsDateProvider")
   private dayjsDateProvider: IDateProvider,
-  @inject("EtherealMailProvider")
+  @inject("MailProvider")
   private mailProvider: IMailProvider
  ) { }
 
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,31 +1,6 @@
-import { container } from "tsyringe";
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
 
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
-import { IStorageProvider } from "./StorageProvider/IStorageProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider"
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-
-container.registerSingleton<IDateProvider>(
- "DayjsDateProvider",
- DayjsDateProvider
-)
-container.registerInstance<IMailProvider>(
- "EtherealMailProvider",
- new EtherealMailProvider()
-)
-
-const diskStorage = {
- local: LocalStorageProvider,
- s3: S3StorageProvider
-}
-
-container.registerSingleton<IStorageProvider>(
- "StorageProvider",
- diskStorage[process.env.disk]
-
-)
